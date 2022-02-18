98015d8e97b4184911ec6a1d1844d986f3b713fb
sattydavid <85852442+sattydavid@users.noreply.github.com>
2022-02-18 17:03:22 -0500

    Add files via upload
    For my last semester In my Advanced Software Development class we were placed into two different scrum teams made up of five people each. My scrum team was instructed to create a  product that is a graphic user interface that is connected to a database that had to be created from scratch. We had 10 weeks to complete this project during 4 different sprint periods. Our product was called Grade Checker and it would be written in C# through a Windows Form Application. This product would be used by students to check their grades for certain classes and see their grade for that class and Semester GPA and Cumulative GPA.  their were 3 different data grid view tables that showed different info. The GUI also has a search/filter function for each of the three data grid views. Look at the attached Grade Checker GUI Design.jpg file to see the design of the GUI  My role for this project was to create the GUI from scratch and then connect the database to windows form app/GUI.

---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace GradeCheckerGUI1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable Student;
+        DataTable Class;
+        DataTable Gpa;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable Student = new DataTable();
+            {
+                {
+
+                    SqlCommand command = new SqlCommand();
+                    SqlConnection Connection = new SqlConnection();
+                    Connection.ConnectionString = @"Data Source=DESKTOP-AGRJKOJ\SQLEXPRESS;Initial Catalog=GradeChecker2;Integrated Security=True";
+                    Connection.Open();
+
+                    command.Connection = Connection;
+                    command.CommandText = "select * from Student";
+
+
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+
+                    da.Fill(Student);
+
+
+
+                    dataGridView1.DataSource = Student;
+
+                    Connection.Close();
+
+
+                }
+                DataTable Class = new DataTable();
+                {
+
+
+
+                    SqlCommand command = new SqlCommand();
+                    SqlConnection Connection = new SqlConnection();
+                    Connection.ConnectionString = @"Data Source=DESKTOP-AGRJKOJ\SQLEXPRESS;Initial Catalog=GradeChecker2;Integrated Security=True";
+                    Connection.Open();
+
+                    command.Connection = Connection;
+
+                    command.CommandText = "select * from Class";
+
+
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+
+                    da.Fill(Class);
+
+
+
+
+                    dataGridView2.DataSource = Class;
+
+                    Connection.Close();
+
+                }
+                DataTable Gpa = new DataTable();
+                {
+                    SqlCommand command = new SqlCommand();
+                    SqlConnection Connection = new SqlConnection();
+                    Connection.ConnectionString = @"Data Source=DESKTOP-AGRJKOJ\SQLEXPRESS;Initial Catalog=GradeChecker2;Integrated Security=True";
+                    Connection.Open();
+
+                    command.Connection = Connection;
+
+                    command.CommandText = "select * from GPA";
+
+
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+
+                    da.Fill(Gpa);
+
+
+
+
+                    dataGridView3.DataSource = Gpa;
+
+                    Connection.Close();
+
+                }
+            }
+        }
+        private void checkconnectionlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchbutton_Click(object sender, EventArgs e)
+        {
+
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = String.Format("FirstLastName like '%" + studentfirstnametextbox.Text + "%'");
+
+
+
+           
+        }
+
+        private void searchbutton2_Click(object sender, EventArgs e)
+        {
+
+
+            (dataGridView2.DataSource as DataTable).DefaultView.RowFilter = String.Format("Course like '%" + classtextbox.Text + "%'");
+
+
+
+        }
+
+        private void findoutgpabutton_Click(object sender, EventArgs e)
+        {
+            (dataGridView3.DataSource as DataTable).DefaultView.RowFilter = "Convert(StudentID, 'System.String') LIKE '" + studentidtextbox.Text + "%'";
+
+           
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void studentfirstnametextbox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void studentlastnametextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void studentgradetextbox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+    }
+
---
+++ GradeCheckerGUI.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace GradeCheckerGUI1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchbutton = new System.Windows.Forms.Button();
+            this.studentfirstnamelabel = new System.Windows.Forms.Label();
+            this.classsubejctlabel = new System.Windows.Forms.Label();
+            this.studentfirstnametextbox = new System.Windows.Forms.TextBox();
+            this.classtextbox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.searchbutton2 = new System.Windows.Forms.Button();
+            this.findoutgpabutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GPAStudentID = new System.Windows.Forms.Label();
+            this.studentidtextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchbutton
+            // 
+            this.searchbutton.Location = new System.Drawing.Point(236, 7);
+            this.searchbutton.Margin = new System.Windows.Forms.Padding(4);
+            this.searchbutton.Name = "searchbutton";
+            this.searchbutton.Size = new System.Drawing.Size(100, 28);
+            this.searchbutton.TabIndex = 0;
+            this.searchbutton.Text = "Search";
+            this.searchbutton.UseVisualStyleBackColor = true;
+            this.searchbutton.Click += new System.EventHandler(this.searchbutton_Click);
+            // 
+            // studentfirstnamelabel
+            // 
+            this.studentfirstnamelabel.AutoSize = true;
+            this.studentfirstnamelabel.Location = new System.Drawing.Point(8, 11);
+            this.studentfirstnamelabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.studentfirstnamelabel.Name = "studentfirstnamelabel";
+            this.studentfirstnamelabel.Size = new System.Drawing.Size(80, 17);
+            this.studentfirstnamelabel.TabIndex = 1;
+            this.studentfirstnamelabel.Text = "First Name:";
+            // 
+            // classsubejctlabel
+            // 
+            this.classsubejctlabel.AutoSize = true;
+            this.classsubejctlabel.Location = new System.Drawing.Point(7, 379);
+            this.classsubejctlabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.classsubejctlabel.Name = "classsubejctlabel";
+            this.classsubejctlabel.Size = new System.Drawing.Size(97, 17);
+            this.classsubejctlabel.TabIndex = 4;
+            this.classsubejctlabel.Text = "Class Subject:";
+            // 
+            // studentfirstnametextbox
+            // 
+            this.studentfirstnametextbox.Location = new System.Drawing.Point(96, 7);
+            this.studentfirstnametextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.studentfirstnametextbox.Name = "studentfirstnametextbox";
+            this.studentfirstnametextbox.Size = new System.Drawing.Size(132, 22);
+            this.studentfirstnametextbox.TabIndex = 7;
+            this.studentfirstnametextbox.TextChanged += new System.EventHandler(this.studentfirstnametextbox_TextChanged);
+            // 
+            // classtextbox
+            // 
+            this.classtextbox.Location = new System.Drawing.Point(113, 375);
+            this.classtextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.classtextbox.Name = "classtextbox";
+            this.classtextbox.Size = new System.Drawing.Size(132, 22);
+            this.classtextbox.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-4, 138);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(827, 210);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(10, 545);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(742, 330);
+            this.dataGridView2.TabIndex = 18;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(821, 613);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.Size = new System.Drawing.Size(706, 166);
+            this.dataGridView3.TabIndex = 19;
+            // 
+            // searchbutton2
+            // 
+            this.searchbutton2.Location = new System.Drawing.Point(253, 373);
+            this.searchbutton2.Margin = new System.Windows.Forms.Padding(4);
+            this.searchbutton2.Name = "searchbutton2";
+            this.searchbutton2.Size = new System.Drawing.Size(100, 28);
+            this.searchbutton2.TabIndex = 20;
+            this.searchbutton2.Text = "Search";
+            this.searchbutton2.UseVisualStyleBackColor = true;
+            this.searchbutton2.Click += new System.EventHandler(this.searchbutton2_Click);
+            // 
+            // findoutgpabutton
+            // 
+            this.findoutgpabutton.Location = new System.Drawing.Point(1071, 465);
+            this.findoutgpabutton.Margin = new System.Windows.Forms.Padding(4);
+            this.findoutgpabutton.Name = "findoutgpabutton";
+            this.findoutgpabutton.Size = new System.Drawing.Size(159, 28);
+            this.findoutgpabutton.TabIndex = 21;
+            this.findoutgpabutton.Text = "Calculate GPA";
+            this.findoutgpabutton.UseVisualStyleBackColor = true;
+            this.findoutgpabutton.Click += new System.EventHandler(this.findoutgpabutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(815, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "The Student Needs to enter their FullName into the Name Textbox Once they have do" +
+    "ne that they should click the search button";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(884, 17);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "The second step the Student needs to complete is to enter their class subject and" +
+    " then click the search button and look at the second table";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(797, 494);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(369, 17);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "The final step for the Student is to  enter their StudentID. ";
+            // 
+            // GPAStudentID
+            // 
+            this.GPAStudentID.AutoSize = true;
+            this.GPAStudentID.Location = new System.Drawing.Point(859, 477);
+            this.GPAStudentID.Name = "GPAStudentID";
+            this.GPAStudentID.Size = new System.Drawing.Size(78, 17);
+            this.GPAStudentID.TabIndex = 25;
+            this.GPAStudentID.Text = "Student ID:";
+            // 
+            // studentidtextbox
+            // 
+            this.studentidtextbox.Location = new System.Drawing.Point(943, 471);
+            this.studentidtextbox.Name = "studentidtextbox";
+            this.studentidtextbox.Size = new System.Drawing.Size(100, 22);
+            this.studentidtextbox.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(834, 528);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(522, 17);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "After you have entered your StudentID click the Calculate GPA to see your results" +
+    "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(796, 511);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(643, 17);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "The StudentID will be provided in the first table when they enter their full name" +
+    " into the Name textbox ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1540, 969);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.studentidtextbox);
+            this.Controls.Add(this.GPAStudentID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.findoutgpabutton);
+            this.Controls.Add(this.searchbutton2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.classtextbox);
+            this.Controls.Add(this.studentfirstnametextbox);
+            this.Controls.Add(this.classsubejctlabel);
+            this.Controls.Add(this.studentfirstnamelabel);
+            this.Controls.Add(this.searchbutton);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchbutton;
+        private System.Windows.Forms.Label studentfirstnamelabel;
+        private System.Windows.Forms.Label classsubejctlabel;
+        private System.Windows.Forms.TextBox studentfirstnametextbox;
+        private System.Windows.Forms.TextBox classtextbox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button searchbutton2;
+        private System.Windows.Forms.Button findoutgpabutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label GPAStudentID;
+        private System.Windows.Forms.TextBox studentidtextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
